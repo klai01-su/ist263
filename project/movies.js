0dbb7d53c5f87ecd1d28db92db4d468316a97f58
klai01-su <klai01@syr.edu>
2023-12-15 19:42:50 -0500

    project

---
+++ project/movies.js
@@ -0,0 +1,12 @@
+const movies = [
+    { title: "Oppenheimer", releaseYear: "2023", genres: ["War", "Historical","Thriller"], rating: 3.5, reviewdate: "2023-12-15" },
+    { title: "Mission: Impossible - Dead Reckoning Part One", releaseYear: "2023", genres: ["Action"], rating: 3.5, reviewdate: "2023-12-15"   },
+    { title: "Spider-Man: Across the Spider-Verse", releaseYear: "2023", genres: ["Action", "Animation"], rating: 4, reviewdate: "2023-12-15"  },
+    { title: "The Whale", releaseYear: "2022", genres: ["Drama"], rating: 3.5, reviewdate: "2023-12-15"  },
+    { title: "Tar", releaseYear: "2022", genres: ["Drama"], rating: 3.5, reviewdate: "2023-12-15"  },
+    { title: "Soul", releaseYear: "2020", genres: ["Animation","Comedy"], rating: 3.5, reviewdate: "2023-12-15"  },
+    { title: "The Wailing", releaseYear: "2016", genres: ["Horror"], rating: 4, reviewdate: "2023-12-05"  },
+    { title: "Nightcrawler", releaseYear: "2014", genres: ["Thriller"], rating: 4.5, reviewdate: "2023-12-15"  },
+    { title: "Whiplash", releaseYear: "2014", genres: ["Drama","Thriller"], rating: 5, reviewdate: "2023-11-05"  },
+    { title: "I Saw the Devil", releaseYear: "2010", genres: ["Thriller","Action"], rating: 5, reviewdate: "2023-11-05"  },
+];
